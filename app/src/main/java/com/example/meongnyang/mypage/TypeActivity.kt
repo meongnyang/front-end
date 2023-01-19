46a71244309cb2d1dbd23583893934ba7883a359
nakyung128 <lnk12888@naver.com>
2023-01-20 04:34:52 +0900

    [fix]: 반려동물 추가 화면 수정

---
+++ app/src/main/java/com/example/meongnyang/mypage/TypeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.meongnyang.mypage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.meongnyang.R
+
+class TypeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mypage_activity_type)
+    }
+}
