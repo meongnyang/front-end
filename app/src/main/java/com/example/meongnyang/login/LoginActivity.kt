a795de74a3246203d49fda74c695747087a7c6ac
nakyung128 <lnk12888@naver.com>
2023-01-21 17:43:12 +0900

    [fix]: 카카오 로그인 기능 보완

--- app/src/main/java/com/example/meongnyang/login/LoginActivity.kt
+++ app/src/main/java/com/example/meongnyang/login/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.result.ActivityResultCallback
@@ -24,6 +25,7 @@ import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
+import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.model.AuthErrorCause
 import com.kakao.sdk.user.UserApiClient
@@ -31,6 +33,7 @@ import com.kakao.sdk.user.UserApiClient
 class LoginActivity : AppCompatActivity() {
     private lateinit var ggBtn: Button
     private lateinit var kkoBtn: Button
+    private lateinit var kakaoCallback: (OAuthToken?, Throwable?) -> Unit
 
     private var auth: FirebaseAuth? = null // 객체의 공유 인스턴스
     private lateinit var client: GoogleSignInClient
@@ -70,9 +73,52 @@ class LoginActivity : AppCompatActivity() {
             }
         }
 
+        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            if (error != null) {
+                when {
+                    error.toString() == AuthErrorCause.AccessDenied.toString() -> {
+                        Toast.makeText(this, "접근이 거부 됨(동의 취소)", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == AuthErrorCause.InvalidClient.toString() -> {
+                        Toast.makeText(this, "유효하지 않은 앱", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == AuthErrorCause.InvalidGrant.toString() -> {
+                        Toast.makeText(this, "인증 수단이 유효하지 않아 인증할 수 없는 상태", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == AuthErrorCause.InvalidRequest.toString() -> {
+                        Toast.makeText(this, "요청 파라미터 오류", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == AuthErrorCause.InvalidScope.toString() -> {
+                        Toast.makeText(this, "유효하지 않은 scope ID", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == AuthErrorCause.Misconfigured.toString() -> {
+                        Toast.makeText(this, "설정이 올바르지 않음(android key hash)", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == AuthErrorCause.ServerError.toString() -> {
+                        Toast.makeText(this, "서버 내부 에러", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == AuthErrorCause.Unauthorized.toString() -> {
+                        Toast.makeText(this, "앱이 요청 권한이 없음", Toast.LENGTH_SHORT).show()
+                    }
+                    else -> { // Unknown
+                        Toast.makeText(this, "기타 에러", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else if (token != null) {
+                Toast.makeText(this, "멍냥백서 가입 성공!", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, NicknameActivity::class.java)
+                startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+                finish()
+            }
+        }
+
         // 카카오 로그인 버튼 클릭했을 시
         kkoBtn.setOnClickListener {
-            kkoSignIn()
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+                UserApiClient.instance.loginWithKakaoTalk(this, callback = callback)
+            } else {
+                UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+            }
         }
     }
 
@@ -90,7 +136,7 @@ class LoginActivity : AppCompatActivity() {
         startActivityForResult(signInIntent, 100)
     }
 
-    private fun kkoSignIn() {
+    /*private fun kkoSignIn() {
         UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
             if (error != null) {
                 when {
@@ -133,7 +179,7 @@ class LoginActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
-    }
+    }*/
 
     private fun moveNextPage() {
         var currentUser = FirebaseAuth.getInstance().currentUser
